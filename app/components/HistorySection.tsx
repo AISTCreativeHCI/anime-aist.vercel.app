cd4334dec5405159bdd95e5d4344f2c2595d156a
Jun Kato <i@junkato.jp>
2025-12-01 19:02:45 +0900

    Add CRONOS logo

--- app/components/HistorySection.tsx
+++ app/components/HistorySection.tsx
@@ -13,19 +13,6 @@ const HistorySection = () => (
         </li>
       ))}
     </ul>
-
-    <footer>
-      <p>
-        本プロジェクトは、
-        <a href="https://www.jst.go.jp/kisoken/cronos/">JST CRONOS</a>{" "}
-        2025年度採択課題「高融合性ツール基盤技術によるアニメ共創環境の実現」
-        （JPMJCS25K1）の支援を受けたものです。
-      </p>
-      <p className="copyright">
-        &copy; 2025 Animāre（アニマーレ） &mdash; AIST, OLM Digital Inc., The
-        University of Tokyo
-      </p>
-    </footer>
   </section>
 );
 
--- app/page.tsx
+++ app/page.tsx
@@ -21,7 +21,8 @@ const IndexPage: NextPage = () => {
         <p>
           <strong>Animāre（アニマーレ）</strong>
           はラテン語で「命を吹き込む」を意味し、アニメーションの語源でもあります。アニメ（"ani"-me）づくりを対象に、しなやかなツ
-          ール群（"ma"-lleable tools）の基盤技術を研究開発し、アニメ制作に関わる多様なクリエータとツール開発者が互いに影響を与え
+          ール群（"ma"-lleable
+          tools）の基盤技術を研究開発し、アニメ制作に関わる多様なクリエータとツール開発者が互いに影響を与え
           合う関係（"re"-ciprocal
           relationship）を築ける環境の実現を目指すプロジェクトです。
         </p>
@@ -34,6 +35,25 @@ const IndexPage: NextPage = () => {
         <img src="/animare_signature-stroke_B.svg" alt="---" />
       </div>
       <HistorySection />
+      <section>
+        <footer>
+          <div className="cronos">
+            <p>
+              本プロジェクトは、
+              <a href="https://www.jst.go.jp/kisoken/cronos/">
+                JST CRONOS
+              </a>{" "}
+              2025年度採択課題「高融合性ツール基盤技術によるアニメ共創環境の実現」
+              （JPMJCS25K1）の支援を受けたものです。
+            </p>
+            <img src="/cronos.svg" alt="[JST CRONOS ロゴ]" />
+          </div>
+          <p className="copyright">
+            &copy; 2025 Animāre（アニマーレ） &mdash; AIST, OLM Digital Inc.,
+            The University of Tokyo
+          </p>
+        </footer>
+      </section>
     </>
   );
 };
