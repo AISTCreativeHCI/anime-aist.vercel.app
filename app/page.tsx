91eab73068f366773fdf46e0018bc81da01fe39e
Jun Kato <i@junkato.jp>
2025-12-27 03:53:28 +0900

    Minor fix to prevent from line breaks

--- app/page.tsx
+++ app/page.tsx
@@ -18,10 +18,8 @@ const IndexPage: NextPage = () => {
         <Header />
         <p>
           <strong>Animāre（アニマーレ）</strong>
-          はラテン語で「命を吹き込む」を意味し、アニメーションの語源でもあります。アニメ（"ani"-me）づくりを対象に、しなやかなツ
-          ール群（"ma"-lleable
-          tools）の基盤技術を研究開発し、アニメ制作に関わる多様なクリエータとツール開発者が互いに影響を与え
-          合う関係（"re"-ciprocal
+          はラテン語で「命を吹き込む」を意味し、アニメーションの語源でもあります。アニメ（"ani"-me）づくりを対象に、しなやかなツール群（"ma"-lleable
+          tools）の基盤技術を研究開発し、アニメ制作に関わる多様なクリエータとツール開発者が互いに影響を与え合う関係（"re"-ciprocal
           relationship）を築ける環境の実現を目指すプロジェクトです。（
           <Link href="/en">English version of the web page is available.</Link>
           ）
