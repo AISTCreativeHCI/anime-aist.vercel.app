702f7fc97e0938c4a05e49d2bae261defe34ff47
Jun Kato <i@junkato.jp>
2025-12-26 21:31:11 +0900

    Minor fix

--- app/en/layout.tsx
+++ app/en/layout.tsx
@@ -4,8 +4,8 @@ import { ClientLayout } from "../components/ClientLayout";
 
 const title = "Animāre | anime.pj.aist.go.jp";
 const description =
-  "Animāre (pronounced ah-nee-MAH-ray) comes from the Latin \"to give life,\" which is also the root of the word \"animation.\" This project sets anime creation as a key application domain to develop foundational techniques for malleable tools and realize an environment where anime creators and tool developers can form reciprocal relationships.";
-const url = "https://anime.pj.aist.go.jp/";
+  'Animāre (pronounced ah-nee-MAH-ray) comes from the Latin "to give life," which is also the root of the word "animation." This project sets anime creation as a key application domain to develop foundational techniques for malleable tools and realize an environment where anime creators and tool developers can form reciprocal relationships.';
+const url = "https://anime.pj.aist.go.jp/en/";
 
 export const metadata: Metadata = {
   title,
